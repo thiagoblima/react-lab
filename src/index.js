c81f2ef9bcfc25ff27967b28ad00a8096713b2a1
Thiago Lima <thitheguy@gmail.com>
2020-09-21 00:10:27 -0300

    React Life Cycles

--- src/index.js
+++ src/index.js
@@ -34,11 +34,16 @@ class Library extends React.Component {
 	state = { 
 		open: true,
 		freeBookmark: false,
-		hiring: true
+		hiring: true,
+		data: [],
+		loading: false
 	}
 
 	componentDidMount() {
-		console.log("The component is now mounted!")
+		this.setState({loading: true})
+		fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
+			.then(data => data.json())
+			.then(data => this.setState({data, loading: false}))
 	}
 
 	componentDidUpdate() {
@@ -55,6 +60,21 @@ class Library extends React.Component {
 		return (
 			<div>
 				{this.state.hiring ? <Hiring /> : <NotHiring />}
+				{this.state.loading 
+					? "loading..."
+					: <div>
+						{this.state.data.map((product, i) => {
+							return (
+								<div key={i}>
+									<h3>Library Product of the Week!</h3>
+									<h4>{product.name}</h4>
+									<img src={product.image} height={100}/>
+								</div>
+							)
+						})}
+						
+					</div>
+				}
 				<h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
 				<button onClick={this.toggleOpenClosed}>Change</button>
 				{books.map(
