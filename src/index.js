42acf9ab359b03dd9687be02fae938db50210b6e
Thiago Lima <thitheguy@gmail.com>
2020-09-20 23:27:31 -0300

    Testing React DOM new app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+let bookList = [
+	{"title": "Hunger", "author": "Roxane Gay", "pages": 320},
+	{"title": "The Sun Also Rises", "author": "Ernest Hemingway", "pages": 260},
+	{"title": "White Teeth", "author": "Zadie Smith", "pages": 480},
+	{"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304}
+]
+
 const Book = ({title, author, pages}) => {
 	return (
 		<section>
@@ -11,17 +18,24 @@ const Book = ({title, author, pages}) => {
 	)
 }
 
-const Library = () => {
+const Library = ({books}) => {
 	return (
 		<div>
-			<Book title="The Sun Also Rises" author="Ernest Hemingway" pages={260}/>
-			<Book title="White Teeth" author="Zadie Smith" pages={480}/>
-			<Book title="Cat's Cradle" author="Kurt Vonnegut" pages={304}/>
+			{books.map(
+				(book, i) => 
+					<Book 
+						key={i}
+						title={book.title} 
+						author={book.author} 
+						pages={book.pages}/>
+			)}
 		</div>
 	)
 }
 
+
+
 render(
-	<Library />, 
+	<Library books={bookList}/>, 
 	document.getElementById('root')
 )
