135ffbd8781c45127863aa4a2bad11982dbc3ae1
Thiago Lima <thitheguy@gmail.com>
2020-09-20 23:53:48 -0300

    Controlling React State

--- src/index.js
+++ src/index.js
@@ -19,11 +19,22 @@ const Book = ({title, author, pages, freeBookmark}) => {
 	)
 }
 
+const Hiring = () => 
+	<div>
+		<p>The library is hiring. Go to www.library.com/jobs for more.</p>
+	</div>
+
+const NotHiring = () => 
+	<div>
+		<p>The library is not hiring. Check back later for more info.</p>
+	</div>
+
 class Library extends React.Component {
 	
 	state = { 
 		open: true,
-		freeBookmark: false
+		freeBookmark: false,
+		hiring: true
 	}
 
 	toggleOpenClosed = () => {
@@ -35,6 +46,7 @@ class Library extends React.Component {
 		const { books } = this.props
 		return (
 			<div>
+				{this.state.hiring ? <Hiring /> : <NotHiring />}
 				<h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
 				<button onClick={this.toggleOpenClosed}>Change</button>
 				{books.map(
