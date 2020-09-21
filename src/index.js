1f678c1b0b4a05e9916a34fde6d40131c36fa9a4
Thiago Lima <thitheguy@gmail.com>
2020-09-20 23:05:07 -0300

    Testing React DOM new app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let skiData = {
+	total: 50,
+	powder: 20,
+	backcountry: 10,
+	goal: 100
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class SkiDayCounter extends Component {
+	getPercent = decimal => {
+		return decimal * 100 + '%'
+	}
+	calcGoalProgress = (total, goal) => {
+		return this.getPercent(total/goal)
+	}
+	render() {
+		const {total, powder, backcountry, goal} = this.props
+		return (
+			<section>
+				<div>
+					<p>Total Days: {total}</p>
+				</div>
+				<div>
+					<p>Powder Days: {powder}</p>
+				</div>
+				<div>
+					<p>Backcountry Days: {backcountry}</p>
+				</div>
+				<div>
+					<p>Goal Progress: {this.calcGoalProgress(total, goal)}</p>
+				</div>
+			</section>
+		)
+	}
+}
+
+render(
+	<SkiDayCounter 
+		total={skiData.total}
+		powder={skiData.powder}
+		backcountry={skiData.backcountry}
+		goal={skiData.goal}/>, 
+	document.getElementById('root')
+)
