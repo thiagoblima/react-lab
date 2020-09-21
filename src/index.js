8a025e34806e17178101d192025075790508af94
Thiago Lima <thitheguy@gmail.com>
2020-09-21 00:45:29 -0300

    React Accessibility

--- src/index.js
+++ src/index.js
@@ -63,12 +63,12 @@ class Library extends React.Component {
 				{this.state.loading 
 					? "loading..."
 					: <div>
-						{this.state.data.map((product, i) => {
+						{this.state.data.map(product => {
 							return (
-								<div key={i}>
+								<div key={product.id}>
 									<h3>Library Product of the Week!</h3>
 									<h4>{product.name}</h4>
-									<img src={product.image} height={100}/>
+									<img alt={product.name} src={product.image} height={100}/>
 								</div>
 							)
 						})}
