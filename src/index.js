6b37731bd01bcc9783cd8f5b1007de864d5066c5
Thiago Lima <thitheguy@gmail.com>
2020-09-20 23:42:18 -0300

    Testing React DOM new app

--- src/index.js
+++ src/index.js
@@ -19,17 +19,20 @@ const Book = ({title, author, pages}) => {
 }
 
 class Library extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			open: false
-		}
+	
+	state = { open: false }
+
+	toggleOpenClosed = () => {
+		this.setState(prevState => ({
+			open: !prevState.open
+		}))
 	}
 	render() {
 		const { books } = this.props
 		return (
 			<div>
 				<h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+				<button onClick={this.toggleOpenClosed}>Change</button>
 				{books.map(
 					(book, i) => 
 						<Book 
